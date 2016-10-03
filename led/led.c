16e05fe817f67eb937f6a7a4d73e9b052c0006ea
songwenkai <elaipu@elaipu.com>
2016-10-03 13:56:29 +0800

    2
    22

--- led/led.c
+++ led/led.c
@@ -1,7 +1,18 @@
 #include<reg51.h>
+sbit led=P2^0;
+void delay()
+{
+	char i,j;
+	for(i=0;i<100;i++)
+	for(j=0;j<100;j++);
+}
 void main()
 {
 	while(1)
 	{
+		led=0;
+		delay();
+		led=1;
+		delay();
 	}
 }
